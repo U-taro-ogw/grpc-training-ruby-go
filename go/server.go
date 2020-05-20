e6404c7854da194b85bbbd5bd29c19201ff945b7
U-taro-ogw <51327986+U-taro-ogw@users.noreply.github.com>
2020-05-21 01:30:14 +0900

    generate go api

---
+++ go/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/U-taro-ogw/grpc-training-ruby-go/go/lib"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8083")
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+		return
+	}
+	grpcServer := grpc.NewServer()
+	training.RegisterTrainingServer(grpcServer, &server{})
+	log.Printf("==========>>>>>>>>>> training server is running!")
+	grpcServer.Serve(listener)
+}
+
+type server struct {}
+
+func (s *server) Hoge(ctx context.Context, req *training.Empty) (*training.HogeResponse, error) {
+	hogeResponse := &training.HogeResponse{
+		language: "Golang",
+	}
+	return hogeResponse, nil
+}
